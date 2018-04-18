40e8755109f8fb0bd73128bb03b7e48b602b9fd2
Test src <59795214@qq.com>
2018-04-18 17:26:59 +0800

    first version

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js', //相对路径
+    output: {
+        path: path.resolve(__dirname, 'build'), //打包文件的输出路径
+        filename: 'bundle.js' //打包文件名
+    }
+}
